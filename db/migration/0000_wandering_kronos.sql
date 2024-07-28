2e0a61687e39f74f753f61c535ff2a4e33e42272
duxmazter46 <becktheerit@gmail.com>
2024-07-28 08:46:07 +0700

    Initial commit

---
+++ db/migration/0000_wandering_kronos.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "todo" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"todo_text" varchar(255) NOT NULL,
+	"is_done" boolean DEFAULT false,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp (3),
+	"tag" varchar(255) DEFAULT 'general',
+	"rank" integer DEFAULT 1
+);
